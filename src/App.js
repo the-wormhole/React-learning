280eed8a1d5cd12c5509e1a815269cbfb5a327b7
cpt-moonbound <nayagarwal@twilio.com>
2024-01-09 21:03:15 +0530

    useState hook usage

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 // import logo from './logo.svg';
 import './App.css';
-import { useState } from 'react';
+import { useState} from 'react';
 
 function App() {     
 
-  const what = useState("joy");
+  const what = useState("joy");//it returns an array, whose first element is the present 
+  // state and the 2nd is the function is the state changing function
+  // const [preState,stateChanger] = useState("joy");
   console.log(what);
   return (
     <div className="App">
