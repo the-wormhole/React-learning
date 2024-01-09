afa97803d9cca95be39a109cbf78e74e8e7f318a
cpt-moonbound <nayagarwal@twilio.com>
2024-01-09 21:00:00 +0530

    useState hook usage

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
-function App() {
+function App() {     
+
+  const what = useState("joy");
+  console.log(what);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>The subject is feeling {what[0]} today!</h1>
+      <button onClick = {() => {what[1]("anxious")}}>
+        anxious
+      </button>
+      <button onClick = {() => {what[1]("joy")}}>
+        joy
+      </button>
+      <button onClick = {() => {what[1]("lovely")}}>
+        lovely
+      </button>
     </div>
+
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import reportWebVitals from './reportWebVitals';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <App test1 = {1} test2 = {2}/>
   </React.StrictMode>
 );
 
