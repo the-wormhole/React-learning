7f3f156519087a139677588ea0671e8f4057c9fa
cpt-moonbound <nayagarwal@twilio.com>
2024-01-10 20:31:59 +0530

    useReducer() react hook used

--- src/App.js
+++ src/App.js
@@ -1,12 +1,27 @@
 // import logo from './logo.svg';
 import './App.css';
-import { useState, useEffect } from 'react';
+import { useState, useEffect,useReducer } from 'react';
+
+function Checker(){
+
+  const [checked, setChecked] = useReducer(
+    checked => !checked,
+    false);
+    
+  return (
+    <div>
+      <input type="checkbox" value="checked" onChange={setChecked}/>
+      <label>
+        {checked ? "Checked" : "Not Checked"}
+      </label>
+    </div> 
+  )
+}
 
 function App() {     
 
   // const [preState,stateChanger] = useState("joy");
   const what = useState("joy"); // see docs for notes
-  
   
   useEffect(() => {   
     console.log(`it's ${what[0]} at the moment!`)
@@ -24,7 +39,9 @@ function App() {
       <button onClick = {() => {what[1]("lovely")}}>
         lovely
       </button>
-    </div>
+
+      <Checker/>
+    </div>  
 
   );
 }
