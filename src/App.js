2cf1842cebc56254a32c6bf185cf585428f5395f
the-wormhole <nayan.agg13@gmail.com>
2024-01-16 23:24:03 +0530

    passing Render functions

--- src/App.js
+++ src/App.js
@@ -1,80 +1,38 @@
 // import logo from './logo.svg';
 import './App.css';
-import { useRef, useState, useEffect } from 'react';
-//import { query } from 'express';
 
-const Gquery = `
-query{
-  allLifts{
-    name
-    status
-    capacity
-    elevationGain
-  }
-}
-`;
+const contacts = [
+  {name:"Nayan", number:102323},
+  {name:"Anivesh", number:102323},
+  {name:"Anubhav", number:102323},
+  {name:"Antriksh", number:102323}
+]
 
-const opts = {
-  method: "POST",
-  headers: {"Content-Type":"application/json"},
-  body: JSON.stringify({Gquery})
-};
+function List({data,renderItem, renderEmpty}){
 
-function LiftsDisp({name,elevationGain,status,capacity}){
-
-  return(
-    <div>
-      <h1>{name}</h1>
-      <p>{elevationGain} {status} {capacity}</p>
-    </div>
-  );
-
-}
-function App() {   
-
-const [data,setData] = useState(null);
-const [loading,setLoading] = useState(false);
-const [error,setError] = useState(null);
-
-useEffect(() => {
-
-  setLoading(true);
-  fetch("https://snowtooth.moonhighway.com",opts)   // Fetch request is throwing an error
-  .then((res) => res.json())  //parses the body and generates 
-  .then(setData)
-  .then(() => setLoading(false))
-  .catch((err) => {
-    console.log(err);
-    setError(err);
-  });
-},[])
-
-if(loading){
-  return(
-    <h1>Loading....</h1>
+  return (!data.length)?renderEmpty:(
+    <ul>
+      {data.map((item) => (
+        <li key={item.name}>
+          {renderItem(item)}
+        </li>
+      ))}
+    </ul>
   )
 }
-if(error){
 
-  return(
-     <pre>{JSON.stringify(error)}</pre>
-    // <p>{error}</p>
-  );
-}
-if(!data)return null;
-console.log(data);
+function App() {   
+
   return (
-      <div>
-          {data.data.allLifts.map((lift) => (
-            <LiftsDisp name = {lift.name} 
-            elevationGain = {lift.elevationGain} 
-            capacity = {lift.capacity}
-            status = {lift.status}
-            />
-          ))}
-      </div>
+    <>
+      <h1>Contact List</h1>
+      <List 
+        data={contacts}
+        renderEmpty = "There is no data"
+        renderItem={(item) => <>{item.name} - {item.number} </>}
+      />
+    </>
   );
-
 }
 
 export default App;
